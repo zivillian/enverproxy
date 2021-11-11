ce2139f93b8271fa0acbb65d726354236fe3ec4d
zivillian <devel@zivillian.de>
2021-11-12 00:31:08 +0100

    - remove graypy - switch from FHEM to MQTT - adjust config

--- enverproxy.py
+++ enverproxy.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 # This is a simple port-forward / proxy for EnvertecBridge
 
-import socket
-import select
-import time
-import sys
-import os
-import errno
-import configparser
 import ast
-import syslog
+import configparser
+import errno
+import json
+import os
+import paho.mqtt.client as mqtt
+import select
+import socket
 import signal
+import sys
+import syslog
+import time
+from datetime import datetime
 from slog import slog
-from FHEM import FHEM
 
 config = configparser.ConfigParser()
 config['internal']={}
 config['internal']['conf_file'] = '/etc/enverproxy.conf'
 config['internal']['section']   = 'enverproxy'
 config['internal']['version']   = '1.3'
-config['internal']['keys']      = "['buffer_size', 'delay', 'listen_port', 'verbosity', 'log_type', 'log_address', 'log_port', 'forward_IP', 'forward_port', 'user', 'password', 'host', 'protocol', 'id2device']"
+config['internal']['keys']      = "['buffer_size', 'delay', 'listen_port', 'verbosity', 'log_type', 'log_address', 'log_port', 'forward_IP', 'forward_port', 'mqttuser', 'mqttpassword', 'mqtthost', 'mqttport']"
 
 
 class Forward:
@@ -31,6 +33,8 @@ class Forward:
         self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def start(self, host, port):
+        if (host == None or port == None or host == 'None'):
+            return False
         try:
             self.forward.connect((host, port))
             return self.forward
@@ -58,21 +62,19 @@ class TheServer:
         self.server.bind((host, port))
         self.server.listen(200)
 
-    def set_fhem_cred(self, host, user, password, id2device, protocol):
-        if protocol == 'https':
-            self.__url = 'https://'
-        else:
-            self.__url = 'http://'
-        self.__url       = self.__url + host + ':8083/fhem?'
-        self.__user      = user
-        self.__password  = password
-        self.__id2device = id2device
+    def connect_mqtt(self, host, user, password, port):
+        self.mqtt = mqtt.Client('enverproxy')
+        if (user != None or password != None):
+            self.mqtt.username_pw_set(user, password)
+        self.mqtt.connect(host, port)
 
     def main_loop(self):
         self.input_list.append(self.server)
+        self.__log.logMsg('Starting mqtt loop', 5)
+        self.mqtt.loop_start()
+        self.__log.logMsg('mqtt loop started', 5)
         while True:
             self.__log.logMsg('Entering main loop', 5)
-            time.sleep(self.__delay)
             ss = select.select
             inputready, outputready, exceptready = ss(self.input_list, [], [])
             self.__log.logMsg('Inputready: ' + str(inputready), 3)
@@ -80,7 +82,7 @@ class TheServer:
                 if self.s == self.server:
                     # proxy server has new connection request
                     self.on_accept()
-                    break
+                    continue
                 # get the data
                 try:
                     self.data = self.s.recv(self.__buffer_size)
@@ -88,7 +90,7 @@ class TheServer:
                     if not self.data or len(self.data) == 0:
                         # Client closed the connection
                         self.on_close(self.s)
-                        break
+                        continue
                     else:
                         self.on_recv()
                 except OSError as e:
@@ -104,18 +106,17 @@ class TheServer:
         self.__log.logMsg('Entering on_accept', 5)
         forward = Forward(self.__log).start(self.__forward_to[0], self.__forward_to[1])
         clientsock, clientaddr = self.server.accept()
+        self.__log.logMsg(str(clientaddr) + ' has connected', 3)
+        self.input_list.append(clientsock)
         if forward:
-            self.__log.logMsg(str(clientaddr) + ' has connected', 2)
-            self.input_list.append(clientsock)
             self.input_list.append(forward)
             self.__log.logMsg('New connection list: ' + str(self.input_list), 5)
             self.channel[clientsock] = forward
             self.channel[forward] = clientsock
             self.__log.logMsg('New channel dictionary: ' + str(self.channel), 5)
         else:
-            self.__log.logMsg("Can't establish connection with remote server.", 2, syslog.LOG_ERR)
-            self.__log.logMsg("Closing connection with client side" + str(clientaddr), 2, syslog.LOG_ERR)
-            clientsock.close()
+            time.sleep(0.4)
+            self.__log.logMsg('New connection list: ' + str(self.input_list), 5)
 
     def on_close(self, in_s):
         self.__log.logMsg('Entering on_close with ' + str(in_s), 5)
@@ -125,33 +126,35 @@ class TheServer:
             # First connection  cannot be closed: proxy listening on its port
             self.__log.logMsg('No connection left to close', 4)
         else:
-            out_s = self.channel[in_s]
             try:
                 self.__log.logMsg('Trying to close ' + str(in_s), 5)
-                self.__log.logMsg(str(in_s.getpeername()) + " has disconnected", 2)
+                self.__log.logMsg(str(in_s.getpeername()) + " has disconnected", 3)
                 # close the connection with client
                 in_s.close()
             except OSError as e:
                 self.__log.logMsg('On_close socket error with ' + str(in_s) + ': ' + str(e), 2, syslog.LOG_ERR)
-            try:
-                self.__log.logMsg('Trying to close ' + str(out_s), 5)
-                self.__log.logMsg('Closing connection to remote server ' + str(out_s.getpeername()), 2)
-                # close the connection with remote server
-                out_s.close()
-            except OSError as e:
-                self.__log.logMsg('On_close socket error with ' + str(out_s) + ': ' + str(e), 2, syslog.LOG_ERR)
-            #remove objects from input_list
             self.input_list.remove(in_s)
-            self.input_list.remove(out_s)
-            self.__log.logMsg('Remaining connection list: ' + str(self.input_list), 5)
-            # delete both objects from channel dict
-            del self.channel[in_s]
-            del self.channel[out_s]
+            if in_s in self.channel:
+                out_s = self.channel[in_s]
+                try:
+                    self.__log.logMsg('Trying to close ' + str(out_s), 5)
+                    self.__log.logMsg('Closing connection to remote server ' + str(out_s.getpeername()), 2)
+                    # close the connection with remote server
+                    out_s.close()
+                except OSError as e:
+                    self.__log.logMsg('On_close socket error with ' + str(out_s) + ': ' + str(e), 2, syslog.LOG_ERR)
+                #remove objects from input_list
+                self.input_list.remove(out_s)
+                self.__log.logMsg('Remaining connection list: ' + str(self.input_list), 5)
+                # delete both objects from channel dict
+                del self.channel[in_s]
+                del self.channel[out_s]
             self.__log.logMsg('Remaining channel dictionary: ' + str(self.channel), 5)
         
     def close_all(self):
         # Close all connections
         self.__log.logMsg('Entering close_all', 5)
+        self.mqtt.loop_stop()
         if len(self.input_list) > 1:
             # First connection cannot be closed: proxy listening on its port
             ilist = self.input_list[1:]
@@ -192,17 +195,9 @@ class TheServer:
     def submit_data(self, wrdata):
         # Can be https as well. Also: if you use another port then 80 or 443 do not forget to add the port number.
         # user and password.
-        fhem_server = FHEM(self.__url, self.__user, self.__password, self.__log)
         for wrdict in wrdata:
-            self.__log.logMsg('Submitting data for converter: ' + str(wrdict['wrid']) + ' to FHEM', 3)
-            values = ['wrid', 'ac', 'dc', 'temp', 'power', 'totalkwh', 'freq']
-            for value in values:
-                if wrdict['wrid'] in self.__id2device:
-                    fhem_cmd = 'set ' + self.__id2device[wrdict['wrid']] + ' ' + value + ' ' + wrdict[value]
-                    self.__log.logMsg('FHEM command: ' + fhem_cmd, 4)
-                    fhem_server.send_command(fhem_cmd)
-                else:
-                    self.__log.logMsg('No FHEM device known for converter ID ' + wrdict['wrid'], 2)
+            self.__log.logMsg('Submitting data for converter: ' + str(wrdict['wrid']) + ' to MQTT', 3)
+            self.mqtt.publish('enverbridge/' + wrdict['wrid'], json.dumps(wrdict))
         self.__log.logMsg('Finished sending to FHEM', 2)
 
     def process_data(self, data):
@@ -236,7 +231,7 @@ class TheServer:
     def on_recv(self):
         data = self.data
         self.__log.logMsg(str(len(data)) + ' bytes in on_recv', 4)
-        self.__log.logMsg('Data as hex: ' + str(data.hex()), 4)
+        self.__log.logMsg('Data received as hex: ' + str(data.hex()), 2)
         if self.s.getsockname()[1] == self.__port:
             # receving data from a client
             self.__log.logMsg('Data is coming from a client', 5)
@@ -244,21 +239,22 @@ class TheServer:
                 # converter initiates connection
                 # create reply packet
                 reply = self.handshake(data)
-                """
                 # This part is simulating handshake with envertecportal.com
                 # Keep disabled if working as proxy between Enverbridge and envertecportal.com
                 self.__log.logMsg('Replying to handshake with data ' + str(reply.hex()), 4)
                 self.s.send(reply)
                 self.__log.logMsg('Reply sent to: ' + str(self.s), 3)
-                """
+                data = json.dumps({"ip":self.s.getpeername()[0], "last_seen":datetime.utcnow().isoformat()})
+                self.mqtt.publish('enverbridge/bridge', data)
             elif data[:6].hex() == '6803d6681004':
                 # payload from converter
                 self.process_data(data)
             else:
                 self.__log.logMsg('Client sent message with unknown content and length ' + str(len(data)), 2, syslog.LOG_ERR)
         # forward data to proxy peer
-        self.channel[self.s].send(data)
-        self.__log.logMsg('Data forwarded to: ' + str(self.channel[self.s]), 3)
+        if self.s in self.channel:
+            self.channel[self.s].send(data)
+            self.__log.logMsg('Data forwarded to: ' + str(self.channel[self.s]), 3)
 
 
 class Signal_handler:
@@ -298,17 +294,17 @@ if __name__ == '__main__':
         log.logMsg('Stopping server', 1)
         sys.exit(1)
     # Process configuration data
-    forward_to  = (config['enverproxy']['forward_IP'], int(config['enverproxy']['forward_port']))
-    delay       = float(config['enverproxy']['delay'])
-    buffer_size = int(config['enverproxy']['buffer_size'])
-    port        = int(config['enverproxy']['listen_port'])
     verbosity   = int(config['enverproxy']['verbosity'])
-    server      = TheServer(host = '', port = port, forward_to = forward_to, delay = delay, buffer_size = buffer_size, log = log)
     log_type    = config['enverproxy']['log_type']
     log_address = config['enverproxy']['log_address']
     log_port    = int(config['enverproxy']['log_port'])
     log         = slog('Envertec Proxy', verbosity, log_type, log_address, log_port)
-    server.set_fhem_cred(config['enverproxy']['host'], config['enverproxy']['user'], config['enverproxy']['password'], ast.literal_eval(config['enverproxy']['id2device']), config['enverproxy']['protocol'])
+    forward_to  = (config['enverproxy']['forward_IP'], int(config['enverproxy']['forward_port']))
+    delay       = float(config['enverproxy']['delay'])
+    buffer_size = int(config['enverproxy']['buffer_size'])
+    port        = int(config['enverproxy']['listen_port'])
+    server      = TheServer(host = '', port = port, forward_to = forward_to, delay = delay, buffer_size = buffer_size, log = log)
+    server.connect_mqtt(config['enverproxy']['mqtthost'], config['enverproxy']['mqttuser'], config['enverproxy']['mqttpassword'], int(config['enverproxy']['mqttport']))
     # Catch SIGTERM signals    
     signal.signal(signal.SIGTERM, Signal_handler(server, log).sigterm_handler)
     # Start proxy server
